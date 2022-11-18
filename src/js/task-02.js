2810a462b76c475abcf7ecf5c4fb6b428786081d
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-18 14:42:14 +0200

    complite task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createListItemEl = (ingredients) => {
+  const listItemEl = ingredients.map((ingredient) => {
+    const ItemEl = document.createElement("li");
+    ItemEl.textContent = ingredient;
+    ItemEl.classList.add("item");
+    return ItemEl;
+  });
+  return listItemEl;
+};
+
+const ingredientsList = document.querySelector("ul#ingredients");
+ingredientsList.append(...createListItemEl(ingredients));
