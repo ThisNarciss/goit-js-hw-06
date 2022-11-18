3417c880820d9725657908ba012df5bd3d715098
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-18 14:18:34 +0200

    complite task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+const categoriesList = document.querySelector("ul#categories");
+// const listEl = categoriesList.children;
+// console.log("Number of categories:", listEl.length);
+
+const listEl = categoriesList.querySelectorAll("li.item");
+console.log("Number of categories:", listEl.length);
+
+listEl.forEach((element) => {
+  const itemTitleEl = element.querySelector("h2");
+  const itemListEl = element.querySelectorAll("li");
+  console.log("Category:", itemTitleEl.textContent);
+  console.log("Elements:", itemListEl.length);
+});
