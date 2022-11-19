9ebb3651c09fb64f8d1d2da7186aaad9f5bd799e
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-19 18:11:24 +0200

    complete task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChgBodyColorEl = document.querySelector("button.change-color");
+
+const textColorValue = document.querySelector("span.color");
+
+buttonChgBodyColorEl.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textColorValue.textContent = getRandomHexColor();
+});
