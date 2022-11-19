21ebb76a1fcdfc03e467e9490da219cb2b96cbc7
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-19 16:08:30 +0200

    complete task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector("input#name-input");
+const spanEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", (event) => {
+  if (event.currentTarget.value === "") {
+    spanEl.textContent = "Anonymous";
+  } else {
+    spanEl.textContent = event.currentTarget.value;
+  }
+});
