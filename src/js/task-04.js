7c8ed527c3102e8bfa533a6ea7e6a8cac16aa16f
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-19 15:54:12 +0200

    complete task 4

---
+++ src/js/task-04.js
@@ -0,0 +1,16 @@
+let counterValue = 0;
+
+const decButton = document.querySelector('button[data-action="decrement"]');
+const incButton = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector("span#value");
+
+const clickBtnDec = decButton.addEventListener("click", () => {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+});
+
+const clickBtnInc = incButton.addEventListener("click", () => {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+});
+console.log(clickBtnInc);
