d2aacbf0008888a8df9ede0262cf629bc83f439c
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-19 17:59:01 +0200

    complete task 8 and repair task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,7 +4,9 @@ textInput.addEventListener("blur", (event) => {
   event.preventDefault();
   if (event.currentTarget.value.length <= event.currentTarget.dataset.length) {
     textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
   } else {
+    textInput.classList.remove("valid");
     textInput.classList.add("invalid");
   }
 });
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const formRef = document.querySelector("form.login-form");
 
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (
+    event.currentTarget.elements.email.value === "" ||
+    event.currentTarget.elements.password.value === ""
+  ) {
+    alert("Для відправки форми мають бути заповнені всі поля!!!");
+  }
+  const userLoginData = {
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+  console.log(userLoginData);
+  event.currentTarget.reset();
+});
