70971418157e0a59ecb3916ff222679ebd7cb0da
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-19 16:56:03 +0200

    complete task 7

---
+++ src/js/task-07.js
@@ -0,0 +1,6 @@
+const inputRef = document.querySelector("input#font-size-control");
+const spanRef = document.querySelector("span#text");
+
+inputRef.addEventListener("input", (event) => {
+  spanRef.style.fontSize = `${event.currentTarget.value}px`;
+});
