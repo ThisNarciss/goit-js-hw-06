47dd2eae081f058cf2cf99e3fb7a9e8df2024af4
Artem Kozlov <109523742+ThisNarciss@users.noreply.github.com>
2022-11-22 09:53:52 +0300

    adds edits in task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -2,13 +2,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let inputEL = document.querySelector("#controls input").value;
-console.log(inputEL);
-const createBtn = document.querySelector("button[data-create]");
+const createBtn = document.querySelector('button[data-create]');
 console.log(createBtn);
-const destroyBtn = document.querySelector("button[data-destroy]");
+const destroyBtn = document.querySelector('button[data-destroy]');
 console.log(destroyBtn);
-const boxesRef = document.querySelector("div#boxes");
+const boxesRef = document.querySelector('div#boxes');
 
 // const createBoxes = (amount) => {
 //   for (let i = 0; i < amount - 1; i += 1) {
@@ -22,30 +20,30 @@ const boxesRef = document.querySelector("div#boxes");
 //   }
 // };
 
-const createBoxes = (amount) => {
-  createBtn.addEventListener("click", () => {
+const createBoxes = () => {
+  createBtn.addEventListener('click', () => {
     const arrayEl = [];
-
-    for (let i = 0; i < amount - 1; i += 1) {
+    let inputEL = document.querySelector('#controls input').valueAsNumber;
+    console.log(inputEL);
+    for (let i = 0; i < inputEL; i += 1) {
       let newEL = `<div style="width: ${30 + i * 10}px; height: ${
         30 + i * 10
       }px ; background-color: ${getRandomHexColor()}"></div>`;
       arrayEl.push(newEL);
       console.log(arrayEl);
     }
-    const arrayString = arrayEl.join("");
-    return boxesRef.insertAdjacentHTML("afterbegin", arrayString);
+    const arrayString = arrayEl.join('');
+    return boxesRef.insertAdjacentHTML('afterbegin', arrayString);
   });
 };
 
-createBoxes(inputEL);
+createBoxes();
 
 // createBtn.addEventListener("click", createBoxes(5));
 
 const destroyBoxes = () => {
-  destroyBtn.addEventListener("click", () => {
-    boxesRef.innerHTML = "";
+  destroyBtn.addEventListener('click', () => {
+    boxesRef.innerHTML = '';
   });
 };
 destroyBoxes();
-console.log(inputEL);
