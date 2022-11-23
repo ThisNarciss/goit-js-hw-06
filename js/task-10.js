63d9f02a3d0cb1b9af9e25cc5ec074947649bc1f
ThisNarciss <tyomaxxx000@gmail.com>
2022-11-23 10:52:31 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -7,31 +7,31 @@ const destroyBtn = document.querySelector('button[data-destroy]');
 const boxesRef = document.querySelector('div#boxes');
 
 // Solution #1
-// const createBoxes = amount => {
-//   for (let i = 0; i < amount; i += 1) {
-//     const newEL = document.createElement('div');
-//     newEL.style.width = `${30 + i * 10}px`;
-//     newEL.style.height = `${30 + i * 10}px`;
-//     newEL.style.backgroundColor = getRandomHexColor();
-//     newEL.classList.add('box-style');
-//     boxesRef.append(newEL);
-//   }
-// };
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i += 1) {
+    const newEL = document.createElement('div');
+    newEL.style.width = `${30 + i * 10}px`;
+    newEL.style.height = `${30 + i * 10}px`;
+    newEL.style.backgroundColor = getRandomHexColor();
+    newEL.classList.add('box-style');
+    boxesRef.append(newEL);
+  }
+};
 
 // Solution #2
-const createBoxes = amount => {
-  const arrayEl = [];
+// const createBoxes = amount => {
+//   const arrayEl = [];
 
-  for (let i = 0; i < amount; i += 1) {
-    const newEL = `<div style="width: ${30 + i * 10}px; height: ${
-      30 + i * 10
-    }px ; background-color: ${getRandomHexColor()}"></div>`;
-    arrayEl.push(newEL);
-  }
+//   for (let i = 0; i < amount; i += 1) {
+//     const newEL = `<div style="width: ${30 + i * 10}px; height: ${
+//       30 + i * 10
+//     }px ; background-color: ${getRandomHexColor()}"></div>`;
+//     arrayEl.push(newEL);
+//   }
 
-  const stringEl = arrayEl.join('');
-  return boxesRef.insertAdjacentHTML('afterbegin', stringEl);
-};
+//   const stringEl = arrayEl.join('');
+//   return boxesRef.insertAdjacentHTML('afterbegin', stringEl);
+// };
 
 createBtn.addEventListener('click', () => {
   const inputEl = document.querySelector('#controls input').valueAsNumber;
