f649067d5b237bf003a4acb3190842e7267a201b
Artem Kozlov <109523742+ThisNarciss@users.noreply.github.com>
2022-11-25 01:00:08 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -7,18 +7,18 @@ const destroyBtn = document.querySelector('button[data-destroy]');
 const boxesRef = document.querySelector('div#boxes');
 
 // Solution #1
-const createBoxes = amount => {
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < amount; i += 1) {
-    const newEL = document.createElement('div');
-    newEL.style.width = `${30 + i * 10}px`;
-    newEL.style.height = `${30 + i * 10}px`;
-    newEL.style.backgroundColor = getRandomHexColor();
-    newEL.classList.add('box-style');
-    fragment.append(newEL);
-  }
-  boxesRef.append(fragment);
-};
+// const createBoxes = amount => {
+//   const fragment = document.createDocumentFragment();
+//   for (let i = 0; i < amount; i += 1) {
+//     const newEL = document.createElement('div');
+//     newEL.style.width = `${30 + i * 10}px`;
+//     newEL.style.height = `${30 + i * 10}px`;
+//     newEL.style.backgroundColor = getRandomHexColor();
+//     newEL.classList.add('box-style');
+//     fragment.append(newEL);
+//   }
+//   boxesRef.append(fragment);
+// };
 
 // Solution #2
 // const createBoxes = amount => {
@@ -34,6 +34,40 @@ const createBoxes = amount => {
 //   const stringEl = arrayEl.join('');
 //   return boxesRef.insertAdjacentHTML('afterbegin', stringEl);
 // };
+
+// Solution 3
+
+// const boxTemplate = (i = 0) => {
+//   return `<div style="width: ${30 + i * 10}px; height: ${
+//     30 + i * 10
+//   }px; background-color: ${getRandomHexColor()};"></div>`;
+// };
+// const createBoxes = amount => {
+//   const result = [...Array(amount).keys()]
+//     .reduce((previous, current) => {
+//       previous.push(boxTemplate(current));
+//       return previous;
+//     }, [])
+//     .join('');
+
+//   boxesRef.insertAdjacentHTML('afterbegin', result);
+// };
+
+// Solution 3 Short
+const createBoxes = amount => {
+  const result = [...Array(amount).keys()]
+    .reduce((previous, current) => {
+      previous.push(
+        `<div style="width: ${30 + current * 10}px; height: ${
+          30 + current * 10
+        }px; background-color: ${getRandomHexColor()};"></div>`
+      );
+      return previous;
+    }, [])
+    .join('');
+
+  boxesRef.insertAdjacentHTML('afterbegin', result);
+};
 
 createBtn.addEventListener('click', () => {
   const inputEl = document.querySelector('#controls input');
